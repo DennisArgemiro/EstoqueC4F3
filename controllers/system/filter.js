e38887bd84a8c048e0a3507fddd42c177b04c388
DennisArgemiro <dennisarg2011@gmail.com>
2022-06-20 02:28:00 -0300

    gitignore

--- controllers/system/filter.js
+++ controllers/system/filter.js
@@ -14,63 +14,63 @@ router.post("/filter", (req, res) => {
     const { nameFilter, filter } = req.body;
     res.send(nameFilter, filter)
 
-//   switch (`${filter}`) {
-//     case "name":
-//       nameFunc(nameFilter);
-//       break;
-//     case "type":
-//       typeFunc(nameFilter);
-//       break;
-//     case "price":
-//       pricefunc(nameFilter);
-//       break;
-//     case "expirationIn":
-//       exprationInFunc(nameFilter);
-//       break;
-//     case "lote":
-//       loteFunc(nameFilter);
-//       break;
-//     case "qtd":
-//       qtdFunc(nameFilter);
-//       break;
-//  }
+  switch (`${filter}`) {
+    case "name":
+      nameFunc(nameFilter);
+      break;
+    case "type":
+      typeFunc(nameFilter);
+      break;
+    case "price":
+      pricefunc(nameFilter);
+      break;
+    case "expirationIn":
+      exprationInFunc(nameFilter);
+      break;
+    case "lote":
+      loteFunc(nameFilter);
+      break;
+    case "qtd":
+      qtdFunc(nameFilter);
+      break;
+ }
 
-//   function nameFunc(aux) {
-//     Products.findAll({ where: { name: `${aux}` } }).then((productData) => {
-//       res.send(productData);
-//     });
-//   }
-//   function typeFunc(aux) {
-//     Products.findAll({ where: { type: `${aux}` } }).then((productData) => {
-//       res.send(productData);
-//     });
-//   }
-//   function pricefunc(aux) {
-//     Products.findAll({ where: { price: `${aux}` } }).then((productData) => {
-//       res.send(productData);
-//     });
-//   }
-//   function exprationInFunc(aux) {
-//     Products.findAll({ where: { expirationIn: `${aux}` } }).then(
-//       (productData) => {
-//         res.send(productData);
-//       }
-//     );
-//   }
-//   function loteFunc() {
-//     Products.findAll({ where: { lote: `${nameFilter}` } }).then(
-//       (productData) => {
-//         res.send(productData);
-//       }
-//     );
-//   }
-//   function qtdFunc() {
-//     Products.findAll({ where: { qtd: `${nameFilter}` } }).then(
-//       (productData) => {
-//         res.send(productData);
-//       }
-//     );
-//   }
+  function nameFunc(aux) {
+    Products.findAll({ where: { name: `${aux}` } }).then((productData) => {
+      res.send(productData);
+    });
+  }
+  function typeFunc(aux) {
+    Products.findAll({ where: { type: `${aux}` } }).then((productData) => {
+      res.send(productData);
+    });
+  }
+  function pricefunc(aux) {
+    Products.findAll({ where: { price: `${aux}` } }).then((productData) => {
+      res.send(productData);
+    });
+  }
+  function exprationInFunc(aux) {
+    Products.findAll({ where: { expirationIn: `${aux}` } }).then(
+      (productData) => {
+        res.send(productData);
+      }
+    );
+  }
+  function loteFunc() {
+    Products.findAll({ where: { lote: `${nameFilter}` } }).then(
+      (productData) => {
+        res.send(productData);
+      }
+    );
+  }
+  function qtdFunc() {
+    Products.findAll({ where: { qtd: `${nameFilter}` } }).then(
+      (productData) => {
+        res.send(productData);
+      }
+    );
+  }
 });
 
 module.exports = router;
